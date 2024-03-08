6a8dbccf6fe85a2f6943af76cb5f6d8ef9a48df8
Dax Davis <daxantz@gmail.com>
2024-03-08 01:14:23 -0500

    created html file and created JS file with the first 2 functions for the game

---
+++ app.js
@@ -0,0 +1,59 @@
+/* 
+computer choice
+init choices
+randomize choices 
+select random choice 
+return choice
+
+*/
+
+const getComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+
+  
+}
+
+/*
+PlayRound
+init player choice
+init cpu choice
+
+compare choices 
+set winner 
+return winner 
+*/
+
+const playRound = (playerSelection, cpuSelection) => {
+    
+   
+
+    if(playerSelection === "rock" && cpuSelection === "paper") {
+        return "You lose! paper beats rock";
+    } else if (playerSelection === "rock" && cpuSelection === "scissors"){
+        return "You win! rock beats scissors";
+    } else if (playerSelection === "rock" && cpuSelection === "rock") {
+        return "You both chose rock, It's a tie";
+    } else if (playerSelection === "paper" && cpuSelection === "scissors") {
+        return "You lose! sissors beats paper";
+    } else if (playerSelection === "paper" && cpuSelection === "rock"){
+        return "You win! paper beats rock";
+    } else if(playerSelection === "paper" && cpuSelection === "paper") {
+        return "You both chose paper, its a tie";
+    } else if (playerSelection === "scissors" && cpuSelection === "rock"){
+        return "You lose! rock beats scissors"
+    } else if (playerSelection === "scissors" && cpuSelection === "paper"){
+        return "You win! scissors beats paper"
+    } else if (playerSelection === "scissors" && cpuSelection === "scissors") {
+        return "You both chose scissors, it's a tie";
+    }
+}
+
+    let playerSelection = "rock";
+    let cpuSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, cpuSelection));
+
+
+
