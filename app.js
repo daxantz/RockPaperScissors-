2fd2e1516b0be1ec7ebb171797c976ba9e97fa92
Dax Davis <daxantz@gmail.com>
2024-03-10 23:07:31 -0400

    added ui and changed original code to use DOM events/methods/manipulation

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const resultContainer = document.querySelector(".results");
+const p = document.createElement("p");
+const scores = document.querySelector(".score");
+const restartBtn = document.querySelector("#restart");
+let playerScore = 0;
+let cpuScore = 0;
+let ties = 0;
+
 /* 
 computer choice
 init choices
@@ -26,28 +37,67 @@ return winner
 */
 
 const playRound = (playerSelection, cpuSelection) => {
-    
-   
-
+    restartBtn.disabled = true;
     if(playerSelection === "rock" && cpuSelection === "paper") {
-        return "You lose! paper beats rock";
+        p.innerText = "You lose! paper beats rock";
+        cpuScore++;
     } else if (playerSelection === "rock" && cpuSelection === "scissors"){
-        return "You win! rock beats scissors";
+        p.innerText = "You win! rock beats scissors";
+        playerScore++;
     } else if (playerSelection === "rock" && cpuSelection === "rock") {
-        return "You both chose rock, It's a tie";
+        const p = document.createElement("p");
+        p.innerText = "You both chose rock, It's a tie";
+        ties++;
     } else if (playerSelection === "paper" && cpuSelection === "scissors") {
-        return "You lose! sissors beats paper";
+        p.innerText = "You lose! sissors beats paper";
+        cpuScore++;
     } else if (playerSelection === "paper" && cpuSelection === "rock"){
-        return "You win! paper beats rock";
+        p.innerText = "You win! paper beats rock";
+        playerScore++;
     } else if(playerSelection === "paper" && cpuSelection === "paper") {
-        return "You both chose paper, its a tie";
+        p.innerText = "You both chose paper, its a tie";
+        ties++;
     } else if (playerSelection === "scissors" && cpuSelection === "rock"){
-        return "You lose! rock beats scissors"
+        p.innerText = "You lose! rock beats scissors"
+        cpuScore++;
     } else if (playerSelection === "scissors" && cpuSelection === "paper"){
-        return "You win! scissors beats paper"
+        p.innerText = "You win! scissors beats paper";
+        playerScore++;
     } else if (playerSelection === "scissors" && cpuSelection === "scissors") {
-        return "You both chose scissors, it's a tie";
+        p.innerText = "You both chose scissors, it's a tie";
+        ties++;
     }
+    updateScore();
+    resultContainer.appendChild(p);
+}
+
+const updateScore = () => {
+    scores.innerText = "Player Score: " + playerScore + " CPU Score: " + cpuScore;
+
+    if(playerScore === 5 && cpuScore < 5) {
+        scores.innerText = "Player is the winner";
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = "true";
+        restartBtn.disabled = false;
+    } else if(cpuScore === 5 && playerScore < 5) {
+        scores.innerText = "Cpu is the winner";
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+        restartBtn.disabled = false;
+    }
+}
+
+const restartGame = () => {
+    playerScore = 0;
+    cpuScore = 0;
+    p.innerText = '';
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+    
+    updateScore();
 }
 
 /*
@@ -57,19 +107,40 @@ playGame()
 
 const playGame = () => {
     let cpuSelection = getComputerChoice();
-    for(let i = 0; i < 5; i++){
-        let playerSelection = prompt("Choose rock, paper, or scissors");
-        console.log(playRound(playerSelection, cpuSelection))
+    // for(let i = 0; i < 5; i++){
+    //     let playerSelection = prompt("Choose rock, paper, or scissors");
+    //     console.log(playRound(playerSelection, cpuSelection))
         
-    }
+    // }
 }
+
+
+
+rockBtn.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+    
+   
+    
+});
+
+paperBtn.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+    
+})
+
+scissorsBtn.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+})
+
+restartBtn.addEventListener("click", () => {
+    restartGame();
+})
+
+
 
 playGame();
 
-//     let playerSelection = "rock";
-//     let cpuSelection = getComputerChoice();
 
-// console.log(playRound(playerSelection, cpuSelection));
 
 
 
